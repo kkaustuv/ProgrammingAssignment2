aee15731aee120bd1d9f4798c494cef02cf3a06c
kkaustuv <k.kumar.kaustuv@gmail.com>
2016-05-23 22:58:43 +0530

    Update cachematrix.R
    KK's changes for Matrix inverse function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makematrix function creates a matrix and store the value in a list
+## functions stores the input matrix ,matrix inverse value assigned from second function
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## use this function to store the output list in a variable and use the variable in the second function
+makeCacheMatrix <- function(x = numeric()) {
+  mi <- NULL
+  set <- function(y) {
+    x <<- y
+    mi <<- NULL
+  }
+  get <- function() x
+  setmtxinv <- function(mtx_inv) mi <<- mtx_inv
+  getmtxinv <- function() mi
+  list(set = set, get = get,
+       setmtxinv = setmtxinv,
+       getmtxinv = getmtxinv)
 }
 
 
-## Write a short comment describing this function
+## functions tries to get the stored matrix inverse value ,if it doesn't get it from envirnment variable then
+# it calculates matrix incerse and  assigns the value to environment variable
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mi <- x$getmtxinv()
+  if(!is.null(mi)) {
+    message("getting cached data")
+    return(mi)
+  }
+  data <- x$get()
+  mi <- solve(data)
+  x$setmtxinv(mi)
+  mi
 }
